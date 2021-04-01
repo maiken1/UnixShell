4179ee7404beca7a5919355d2a210512ae316e25
Oliver Dahl <oliverpdahl@gmail.com>
2021-04-01 11:18:24 -0400

    command table consistent formating

--- command.h
+++ command.h
@@ -1,46 +1,48 @@
-
 #pragma once
 
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <map>
+
 #include <string>
 
 // #include <vector>
 // std::vector<char *> alias_list;
 // Command Data Structure
 struct SimpleCommand {
-	// Available space for arguments currently preallocated
-	int _numberOfAvailableArguments;
+  // Available space for arguments currently preallocated
+  int _numberOfAvailableArguments;
 
-	// Number of arguments
-	int _numberOfArguments;
-	char ** _arguments;
-	
-	SimpleCommand();
-	void insertArgument( char * argument );
+  // Number of arguments
+  int _numberOfArguments;
+  char ** _arguments;
+
+  SimpleCommand();
+  void insertArgument(char * argument);
 };
 
 struct Command {
-	int _numberOfAvailableSimpleCommands;
-	int _numberOfSimpleCommands;
-	SimpleCommand ** _simpleCommands;
-	char * _outFile;
-	char * _inputFile;
-	char * _errFile;
-	int _background;
+  int _numberOfAvailableSimpleCommands;
+  int _numberOfSimpleCommands;
+  SimpleCommand ** _simpleCommands;
+  char * _outFile;
+  char * _inputFile;
+  char * _errFile;
+  int _background;
 
-	void prompt();
-	void print();
-	void execute();
-	void clear();
-	int CheckNumberOfArguments(char* command, int inputArguments, int min, int max);
-	int search(char * file, char * const argv[]);
-	
-	Command();
-	void insertSimpleCommand( SimpleCommand * simpleCommand );
+  void prompt();
+  void print();
+  void execute();
+  void clear();
+  int CheckNumberOfArguments(char * command, int inputArguments, int min, int max);
+  int search(char * file, char *
+    const argv[]);
 
-	static Command _currentCommand;
-	static SimpleCommand *_currentSimpleCommand;
+  Command();
+  void insertSimpleCommand(SimpleCommand * simpleCommand);
+
+  static Command _currentCommand;
+  static SimpleCommand * _currentSimpleCommand;
 };
-
