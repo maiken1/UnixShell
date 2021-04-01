834111d91a83aa6f7d939739433ec1e9f5d98ff1
maiken1 <57634158+maiken1@users.noreply.github.com>
2021-04-01 15:58:04 -0400

    Added error file redirection and now removed quotes from string literals

--- nutshell.cpp
+++ nutshell.cpp
@@ -160,6 +160,7 @@ Command::execute() {
 
     int defaultin = dup(0);
     int defaultout = dup(1);
+    int defaulterror = dup(2);
 
     int fdin;
     if (_inputFile) {
@@ -171,6 +172,7 @@ Command::execute() {
 
     pid_t pid;
     int fdout;
+    int fderr;
     for (int i = 0; i < _numberOfSimpleCommands; i++) {
       //redirect input
       dup2(fdin, 0);
@@ -192,6 +194,13 @@ Command::execute() {
           fdout = dup(defaultout);
           // printf("I am the last command\n");
         }
+        if (_errFile) {
+            fderr = open(_errFile, O_CREAT | O_WRONLY, 0777);
+        }
+        else {
+            //default error
+            fderr = dup(defaulterror);
+        }
       } else {
         //not last simple command
         printf("creating pipes\n");
@@ -202,7 +211,9 @@ Command::execute() {
       }
       //redirect output
       dup2(fdout, 1);
+      dup2(fderr, 2);
       close(fdout);
+      close(fderr);
       //may have to deal with this for backgrounds
       int status;
       string builtinCheck(_simpleCommands[i] -> _arguments[0]);
@@ -241,7 +252,7 @@ Command::execute() {
               printf("%s\n", out.c_str());
             }
           } else {
-            printf("alias: Wrong number of arguments\n");
+            perror("alias: Wrong number of arguments\n");
           }
         }
       } else if (builtinCheck == "unalias") {
@@ -276,8 +287,10 @@ Command::execute() {
     //restore in/out states to default
     dup2(defaultin, 0);
     dup2(defaultout, 1);
+    dup2(defaulterror, 2);
     close(defaultin);
     close(defaultout);
+    close(defaulterror);
     // printf("defaults restored\n");
 
     if (!_background) {
