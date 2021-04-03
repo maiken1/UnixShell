f85bee85aa6c961bcbf1cc468e7c405e17e316e4
Oliver Dahl <oliverpdahl@gmail.com>
2021-04-03 10:08:52 -0400

    Removed trash files

--- global.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <string.h>
-#include <unistd.h>
-#include <pwd.h>
-#include <glob.h>
-#include <signal.h>
